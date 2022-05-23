670cc44959c20e92c8f9a737200518cd55581ba5
ddanny16 <dammydan16@gmail.com>
2022-05-23 14:33:03 +0200

    ...

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// const ham = document.querySelector('.ham')
-// const mobileNav = document.querySelector('.mobile_nav')
-// const closeMenu = document.querySelector('.closemenu')
-// const blurOverlay =document.querySelector('.blur_overlay')
-// const IS_ACTIVE = 'is--active'
+const ham = document.querySelector('.ham')
+const mobileNav = document.querySelector('.mobile_nav')
+const closeMenu = document.querySelector('.closemenu')
+const blurOverlay =document.querySelector('.blur_overlay')
+const IS_ACTIVE = 'is--active'
 
 
 
-// const toggleNavigation = () =>{
-//     mobileNav.classList.toggle(IS_ACTIVE)
-//     blurOverlay.classList.toggle(IS_ACTIVE)
-// }
+const toggleNavigation = () =>{
+    mobileNav.classList.toggle(IS_ACTIVE)
+    blurOverlay.classList.toggle(IS_ACTIVE)
+}
 
-// const click ='click'
+const click ='click'
 
-// ham.addEventListener(click , toggleNavigation)
-// closeMenu.addEventListener(click , toggleNavigation)
-// blurOverlay.addEventListener(click , toggleNavigation)
+ham.addEventListener(click , toggleNavigation)
+closeMenu.addEventListener(click , toggleNavigation)
+blurOverlay.addEventListener(click , toggleNavigation)
